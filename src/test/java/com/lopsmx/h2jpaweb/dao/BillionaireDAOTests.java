f1d8c616bf414724579690485d7cbbeafe8ac435
Alex Villagran <alex.villagran.mancilla@gmail.com>
2021-05-15 00:21:35 -0500

    Se eliminaron comentarios

--- src/test/java/com/lopsmx/h2jpaweb/dao/BillionaireDAOTests.java
+++ src/test/java/com/lopsmx/h2jpaweb/dao/BillionaireDAOTests.java
@@ -7,26 +7,17 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
-
-
-
-/****NO ESTA 100% TERMINADO */
-
-//@RunWith(SpringRunner.class)
 @SpringBootTest
 public class BillionaireDAOTests {
 	
 	private static final Logger log = LoggerFactory.getLogger(BillionaireDAOTests.class);
-
 	
 	@Autowired
 	BillionaireDAO billionaireDAO; 
 	
 	@Test
 	public void findAllBillionaries() {
-		log.info("***** Find Billionaries *****");
-//		Iterable<Billionaire> iterable = billionaireDAO.findAll();
-		
+		log.info("***** Find Billionaries *****");		
 		
 		billionaireDAO.findAll().forEach(billionarie -> {
 			log.info(billionarie.toString());
